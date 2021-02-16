6551ddb89907dce8f472fe8f02f789825063a2da
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-02-15 19:24:18 -0800

    Create two-sum.swift
    1. Two Sum

---
+++ algorithms/swift/two-sum.swift
@@ -0,0 +1,57 @@
+
+// Source : https://leetcode.com/problems/two-sum/
+
+/**************************************************************************************************
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ You can return the answer in any order.
+
+  
+
+ Example 1:
+
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ Example 2:
+
+ Input: nums = [3,2,4], target = 6
+ Output: [1,2]
+ Example 3:
+
+ Input: nums = [3,3], target = 6
+ Output: [0,1]
+  
+
+ Constraints:
+
+ 2 <= nums.length <= 103
+ -109 <= nums[i] <= 109
+ -109 <= target <= 109
+ Only one valid answer exists.
+ **************************************************************************************************/
+
+
+import Foundation
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var result: [Int] = []
+        var diff: Int = 0
+        
+        for (index, num) in nums.enumerated() {
+            diff = target - num
+          
+            if let indexOfDiff = nums.firstIndex(of: diff), index != indexOfDiff, !result.contains(index){
+                
+                result.append(index)
+                result.append(indexOfDiff)
+            }
+        }
+        
+        return result
+    }
+}
