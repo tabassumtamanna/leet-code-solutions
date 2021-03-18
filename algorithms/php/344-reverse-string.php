ac54a02bc8bfea9ef5f3c3f6a4bc6c2864da0901
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-03-17 17:23:39 -0700

    add submissions

---
+++ algorithms/php/344-reverse-string.php
@@ -0,0 +1,53 @@
+// 344. Reverse String
+// Source : https://leetcode.com/problems/reverse-string/
+
+/**************************************************************************************************
+ Write a function that reverses a string. The input string is given as an array of characters s.
+
+  
+
+ Example 1:
+
+ Input: s = ["h","e","l","l","o"]
+ Output: ["o","l","l","e","h"]
+ Example 2:
+
+ Input: s = ["H","a","n","n","a","h"]
+ Output: ["h","a","n","n","a","H"]
+  
+
+ Constraints:
+
+ 1 <= s.length <= 105
+ s[i] is a printable ascii character.
+  
+
+ Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+ **************************************************************************************************/
+
+<?php
+    
+class Solution {
+
+    /**
+     * @param String[] $s
+     * @return NULL
+     */
+    function reverseString(&$s) {
+        
+        $temp = "";
+        $length = count($s)-1;
+        
+        for($i=0; $i< $length/2; $i++){
+            
+            $temp = $s[$i];
+            $s[$i] = $s[$length -$i];
+            $s[$length-$i] = $temp;
+        }
+        
+        
+    }
+}
+?>
+
