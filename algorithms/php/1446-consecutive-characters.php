a0d172c3f5a5fe1df954ce14e7c4cd12d142b1d3
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-04-25 22:29:14 -0700

    Create 1446-consecutive-characters.php

---
+++ algorithms/php/1446-consecutive-characters.php
@@ -0,0 +1,74 @@
+// 1446. Consecutive Characters
+// Source : https://leetcode.com/problems/consecutive-characters/
+
+/**************************************************************************************************
+ Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+ Return the power of the string.
+
+  
+
+ Example 1:
+
+ Input: s = "leetcode"
+ Output: 2
+ Explanation: The substring "ee" is of length 2 with the character 'e' only.
+ Example 2:
+
+ Input: s = "abbcccddddeeeeedcba"
+ Output: 5
+ Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ Example 3:
+
+ Input: s = "triplepillooooow"
+ Output: 5
+ Example 4:
+
+ Input: s = "hooraaaaaaaaaaay"
+ Output: 11
+ Example 5:
+
+ Input: s = "tourist"
+ Output: 1
+  
+
+ Constraints:
+
+ 1 <= s.length <= 500
+ s contains only lowercase English letters.
+  
+ **************************************************************************************************/
+
+<?php
+
+    
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function maxPower($s) {
+        
+        $count = 0;
+        $maxCount = 0;
+        $prev = "";
+        
+        $arr = str_split($s);
+        for($i=0; $i<count($arr); $i++){
+            
+            if($arr[$i] == $prev){
+                $count++;
+            } else {
+                $count = 1;
+                $prev = $arr[$i];
+            }
+            
+            $maxCount = max($maxCount, $count);
+        }
+        
+        return $maxCount;
+    }
+}
+
+?>
