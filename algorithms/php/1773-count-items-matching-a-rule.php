9580b4760d778e4847596d5acf9b2b065300d68d
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-05-05 22:07:05 -0700

    Create 1773-count-items-matching-a-rule.php

---
+++ algorithms/php/1773-count-items-matching-a-rule.php
@@ -0,0 +1,67 @@
+// 1773. Count Items Matching a Rule
+// Source : https://leetcode.com/problems/count-items-matching-a-rule/
+
+/**************************************************************************************************
+ You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+ The ith item is said to match the rule if one of the following is true:
+
+ ruleKey == "type" and ruleValue == typei.
+ ruleKey == "color" and ruleValue == colori.
+ ruleKey == "name" and ruleValue == namei.
+ Return the number of items that match the given rule.
+
+  
+
+ Example 1:
+
+ Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+ Output: 1
+ Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+ Example 2:
+
+ Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+ Output: 2
+ Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+  
+
+ Constraints:
+
+ 1 <= items.length <= 104
+ 1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+ ruleKey is equal to either "type", "color", or "name".
+ All strings consist only of lowercase letters.
+
+  
+ **************************************************************************************************/
+
+<?php
+
+class Solution {
+
+    /**
+     * @param String[][] $items
+     * @param String $ruleKey
+     * @param String $ruleValue
+     * @return Integer
+     */
+    function countMatches($items, $ruleKey, $ruleValue) {
+        
+        switch ($ruleKey){
+            case "type": $index = 0; break;
+            case "color": $index = 1;  break;
+            case "name": $index = 2;  break;
+        }
+        
+        $count = 0;
+        foreach($items as $item){
+            if( $item[$index] == $ruleValue)
+                $count++;
+        }
+        
+        return $count;
+    }
+}
+    
+    
+?>
