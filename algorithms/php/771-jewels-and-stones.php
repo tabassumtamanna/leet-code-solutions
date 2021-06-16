17a7b1eaf50b7d407bdd42e669d6d10ee102018b
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-06-15 20:03:51 -0700

    Create 771-jewels-and-stones.php

---
+++ algorithms/php/771-jewels-and-stones.php
@@ -0,0 +1,56 @@
+// 771. Jewels and Stones
+// Source : https://leetcode.com/problems/jewels-and-stones/
+
+/**************************************************************************************************
+ You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+ Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+  
+
+ Example 1:
+
+ Input: jewels = "aA", stones = "aAAbbbb"
+ Output: 3
+ Example 2:
+
+ Input: jewels = "z", stones = "ZZ"
+ Output: 0
+  
+
+ Constraints:
+
+ 1 <= jewels.length, stones.length <= 50
+ jewels and stones consist of only English letters.
+ All the characters of jewels are unique.
+  
+ **************************************************************************************************/
+
+<?php
+  
+class Solution {
+
+    /**
+     * @param String $J
+     * @param String $S
+     * @return Integer
+     */
+    function numJewelsInStones($J, $S) {
+        
+        $stoneList = str_split($S);
+        
+        $count = 0;
+        for($i=0; $i<count($stoneList); $i++){
+            
+            $pos = strpos($J, $stoneList[$i]);
+            
+            if($pos !== false){
+                $count++;
+            }
+            
+        }
+        
+        return $count;
+    }
+}
+?>
